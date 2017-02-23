00c8d4f54126bf1f85aa0a5b807952c88b87e439
alex23vazquez <alex23_vazquez@hotmail.com>
2017-02-22 20:26:02 -0600

    Update Four_Wheel_Drive.ino

--- DustyTron3_MainCode/DustyTron3_MainCode/Four_Wheel_Drive/Four_Wheel_Drive.ino
+++ DustyTron3_MainCode/DustyTron3_MainCode/Four_Wheel_Drive/Four_Wheel_Drive.ino
@@ -19,27 +19,6 @@ void victor(int c, int A, int B){
 
 void wheelSpeed(int CASE, int SPEED, int PWM){
   switch(CASE){
-    case 0://increment speed
-      if(SPEED == 255){
-      analogWrite(PWM, SPEED);
-      }else{
-      spd += 15; //Speed increment of 85 (3 times)
-      SPEED = spd;
-      analogWrite(PWM, SPEED);
-      }
-      break;
-      
-    case 1://decrement speed
-      if(SPEED == 0){
-      analogWrite(PWM, SPEED);
-      }else{
-      spd -= 15;
-      SPEED = spd;//Speed decrement of 85 (3 times)
-      
-      analogWrite(PWM, SPEED);
-      }
-      break; 
-
     case 2: //decrement speed , I AM NOT SURE WHY IT SAYS decrement speed, might be a mistake
       analogWrite(PWM, SPEED);
       Serial.print(SPEED);

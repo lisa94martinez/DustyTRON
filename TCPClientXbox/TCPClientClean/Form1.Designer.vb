e09c1590acb2e1ef8b08e69869397b8e109d5e48
lisa_martinez <lisa_martinez@dusty.tamiu.edu>
2017-02-27 00:34:01 -0600

    initial commint of second controller

--- TCPClientXbox/TCPClientClean/Form1.Designer.vb
+++ TCPClientXbox/TCPClientClean/Form1.Designer.vb
@@ -44,6 +44,7 @@ Partial Class Form1
         Me.Label26 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -55,10 +56,9 @@ Partial Class Form1
         '
         Me.PictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(391, 13)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.PictureBox1.Location = New System.Drawing.Point(293, 11)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(678, 362)
+        Me.PictureBox1.Size = New System.Drawing.Size(508, 294)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 4
         Me.PictureBox1.TabStop = False
@@ -67,10 +67,9 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.BackColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(877, 79)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(658, 64)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(152, 17)
+        Me.Label1.Size = New System.Drawing.Size(115, 13)
         Me.Label1.TabIndex = 6
         Me.Label1.Text = "Increase Global Speed"
         '
@@ -78,10 +77,9 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.BackColor = System.Drawing.Color.White
-        Me.Label2.Location = New System.Drawing.Point(877, 106)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Location = New System.Drawing.Point(658, 86)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(185, 17)
+        Me.Label2.Size = New System.Drawing.Size(142, 13)
         Me.Label2.TabIndex = 7
         Me.Label2.Text = "Excavate Auger High Speed"
         '
@@ -89,10 +87,9 @@ Partial Class Form1
         '
         Me.Label6.AutoSize = True
         Me.Label6.BackColor = System.Drawing.Color.White
-        Me.Label6.Location = New System.Drawing.Point(868, 216)
-        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Location = New System.Drawing.Point(651, 176)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(68, 17)
+        Me.Label6.Size = New System.Drawing.Size(53, 13)
         Me.Label6.TabIndex = 11
         Me.Label6.Text = "Kill switch"
         '
@@ -100,10 +97,9 @@ Partial Class Form1
         '
         Me.Label8.AutoSize = True
         Me.Label8.BackColor = System.Drawing.Color.White
-        Me.Label8.Location = New System.Drawing.Point(404, 79)
-        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Location = New System.Drawing.Point(303, 64)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(159, 17)
+        Me.Label8.Size = New System.Drawing.Size(120, 13)
         Me.Label8.TabIndex = 14
         Me.Label8.Text = "Decrease Global Speed"
         '
@@ -111,10 +107,9 @@ Partial Class Form1
         '
         Me.Label9.AutoSize = True
         Me.Label9.BackColor = System.Drawing.Color.White
-        Me.Label9.Location = New System.Drawing.Point(404, 106)
-        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Location = New System.Drawing.Point(303, 86)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(165, 17)
+        Me.Label9.Size = New System.Drawing.Size(125, 13)
         Me.Label9.TabIndex = 15
         Me.Label9.Text = "Dump Auger High Speed"
         '
@@ -122,10 +117,9 @@ Partial Class Form1
         '
         Me.Label11.AutoSize = True
         Me.Label11.BackColor = System.Drawing.Color.White
-        Me.Label11.Location = New System.Drawing.Point(457, 161)
-        Me.Label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label11.Location = New System.Drawing.Point(343, 131)
         Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(134, 34)
+        Me.Label11.Size = New System.Drawing.Size(103, 26)
         Me.Label11.TabIndex = 17
         Me.Label11.Text = "Up (Auger Up)/ " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Down (Auger Down)"
         '
@@ -133,10 +127,9 @@ Partial Class Form1
         '
         Me.Label12.AutoSize = True
         Me.Label12.BackColor = System.Drawing.Color.White
-        Me.Label12.Location = New System.Drawing.Point(448, 199)
-        Me.Label12.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label12.Location = New System.Drawing.Point(336, 162)
         Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(143, 34)
+        Me.Label12.Size = New System.Drawing.Size(106, 26)
         Me.Label12.TabIndex = 18
         Me.Label12.Text = "Left(Auger Tilt In)/" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Right (Auger Tilt Out)"
         '
@@ -144,31 +137,27 @@ Partial Class Form1
         '
         Me.Label4.AutoSize = True
         Me.Label4.BackColor = System.Drawing.Color.White
-        Me.Label4.Location = New System.Drawing.Point(703, 39)
-        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Location = New System.Drawing.Point(527, 32)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(66, 17)
+        Me.Label4.Size = New System.Drawing.Size(51, 13)
         Me.Label4.TabIndex = 19
         Me.Label4.Text = "Driver #1"
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 16
-        Me.ListBox1.Location = New System.Drawing.Point(584, 421)
-        Me.ListBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ListBox1.Location = New System.Drawing.Point(438, 342)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(284, 180)
+        Me.ListBox1.Size = New System.Drawing.Size(214, 147)
         Me.ListBox1.TabIndex = 30
         '
         'Label21
         '
         Me.Label21.AutoSize = True
         Me.Label21.BackColor = System.Drawing.Color.White
-        Me.Label21.Location = New System.Drawing.Point(868, 189)
-        Me.Label21.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label21.Location = New System.Drawing.Point(651, 154)
         Me.Label21.Name = "Label21"
-        Me.Label21.Size = New System.Drawing.Size(181, 17)
+        Me.Label21.Size = New System.Drawing.Size(136, 13)
         Me.Label21.TabIndex = 31
         Me.Label21.Text = "Dump Auger Normal Speed"
         '
@@ -176,10 +165,9 @@ Partial Class Form1
         '
         Me.Label22.AutoSize = True
         Me.Label22.BackColor = System.Drawing.Color.White
-        Me.Label22.Location = New System.Drawing.Point(868, 247)
-        Me.Label22.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label22.Location = New System.Drawing.Point(651, 201)
         Me.Label22.Name = "Label22"
-        Me.Label22.Size = New System.Drawing.Size(201, 17)
+        Me.Label22.Size = New System.Drawing.Size(153, 13)
         Me.Label22.TabIndex = 32
         Me.Label22.Text = "Excavate Auger Normal Speed"
         '
@@ -187,10 +175,9 @@ Partial Class Form1
         '
         Me.Label23.AutoSize = True
         Me.Label23.BackColor = System.Drawing.Color.White
-        Me.Label23.Location = New System.Drawing.Point(457, 247)
-        Me.Label23.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label23.Location = New System.Drawing.Point(343, 201)
         Me.Label23.Name = "Label23"
-        Me.Label23.Size = New System.Drawing.Size(124, 17)
+        Me.Label23.Size = New System.Drawing.Size(94, 13)
         Me.Label23.TabIndex = 33
         Me.Label23.Text = "Straighten Wheels"
         '
@@ -198,10 +185,9 @@ Partial Class Form1
         '
         Me.Label24.AutoSize = True
         Me.Label24.BackColor = System.Drawing.Color.White
-        Me.Label24.Location = New System.Drawing.Point(472, 274)
-        Me.Label24.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label24.Location = New System.Drawing.Point(354, 223)
         Me.Label24.Name = "Label24"
-        Me.Label24.Size = New System.Drawing.Size(109, 17)
+        Me.Label24.Size = New System.Drawing.Size(83, 13)
         Me.Label24.TabIndex = 34
         Me.Label24.Text = "Dump Conveyor"
         '
@@ -209,10 +195,9 @@ Partial Class Form1
         '
         Me.Label25.AutoSize = True
         Me.Label25.BackColor = System.Drawing.Color.White
-        Me.Label25.Location = New System.Drawing.Point(456, 298)
-        Me.Label25.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label25.Location = New System.Drawing.Point(342, 242)
         Me.Label25.Name = "Label25"
-        Me.Label25.Size = New System.Drawing.Size(125, 17)
+        Me.Label25.Size = New System.Drawing.Size(95, 13)
         Me.Label25.TabIndex = 35
         Me.Label25.Text = "Reverse Conveyor"
         '
@@ -220,10 +205,9 @@ Partial Class Form1
         '
         Me.Label26.AutoSize = True
         Me.Label26.BackColor = System.Drawing.Color.White
-        Me.Label26.Location = New System.Drawing.Point(495, 327)
-        Me.Label26.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label26.Location = New System.Drawing.Point(371, 266)
         Me.Label26.Name = "Label26"
-        Me.Label26.Size = New System.Drawing.Size(93, 17)
+        Me.Label26.Size = New System.Drawing.Size(71, 13)
         Me.Label26.TabIndex = 36
         Me.Label26.Text = "Steer Wheels"
         '
@@ -231,10 +215,9 @@ Partial Class Form1
         '
         Me.Label5.AutoSize = True
         Me.Label5.BackColor = System.Drawing.Color.White
-        Me.Label5.Location = New System.Drawing.Point(868, 293)
-        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Location = New System.Drawing.Point(651, 238)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(118, 34)
+        Me.Label5.Size = New System.Drawing.Size(92, 26)
         Me.Label5.TabIndex = 37
         Me.Label5.Text = "Up (Foward)/ " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Down (Backward)"
         '
@@ -242,18 +225,26 @@ Partial Class Form1
         '
         Me.Label3.AutoSize = True
         Me.Label3.BackColor = System.Drawing.Color.White
-        Me.Label3.Location = New System.Drawing.Point(868, 327)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Location = New System.Drawing.Point(651, 266)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(126, 34)
+        Me.Label3.Size = New System.Drawing.Size(94, 26)
         Me.Label3.TabIndex = 38
         Me.Label3.Text = "Left(Steer Left)/" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Right (Steer Right)"
         '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(654, 360)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(214, 147)
+        Me.ListBox2.TabIndex = 39
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1531, 676)
+        Me.ClientSize = New System.Drawing.Size(1148, 549)
+        Me.Controls.Add(Me.ListBox2)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label26)
@@ -272,7 +263,6 @@ Partial Class Form1
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form1"
         Me.Text = "Client"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -300,4 +290,5 @@ Partial Class Form1
     Friend WithEvents Label26 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label3 As Label
+    Friend WithEvents ListBox2 As ListBox
 End Class
